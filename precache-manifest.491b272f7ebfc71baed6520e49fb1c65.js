66a48681eca44f883b82b8dd8348a428ead4227f
kant0 <1195510518@qq.com>
2020-11-29 17:22:06 +0800

    deploy

---
+++ precache-manifest.491b272f7ebfc71baed6520e49fb1c65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43483e09486c2cf98e54",
+    "url": "/Money-1-website/css/app.3ef6f3d8.css"
+  },
+  {
+    "revision": "2cdc0525cff50877200f0d8f798b0dca",
+    "url": "/Money-1-website/index.html"
+  },
+  {
+    "revision": "43483e09486c2cf98e54",
+    "url": "/Money-1-website/js/app.67ba6335.js"
+  },
+  {
+    "revision": "e72189765938c5bdb86b",
+    "url": "/Money-1-website/js/chunk-vendors.562ba9c7.js"
+  },
+  {
+    "revision": "03d149c16808b83a78098bee231d70f8",
+    "url": "/Money-1-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Money-1-website/robots.txt"
+  }
+]);
